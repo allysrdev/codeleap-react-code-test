ac3c731bbae76382b7f50641d8d22183dfe86922
Ally Santana <contato.allysantana@gmail.com>
2026-03-12 17:10:09 -0300

    feature: adding simple SignOut solution

--- src/components/ui/Layout/index.tsx
+++ src/components/ui/Layout/index.tsx
@@ -1,13 +1,25 @@
+import { SignOutIcon } from "@phosphor-icons/react";
 import { Outlet } from "react-router-dom";
 import { Toaster } from "sonner";
+import { useUser } from "../../../features/auth/hooks/useUser";
 
 export default function Layout() {
+  const { setUser } = useUser();
+  function handleSignOut() {
+    setUser(null);
+  }
   return (
     <div data-testid="layout" className="min-h-screen flex justify-center">
       <Toaster position="top-right" richColors />
       <div className="w-full min-h-screen sm:max-w-[50%] flex flex-col">
-        <header className="bg-[#7695EC] p-8!">
+        <header className="bg-[#7695EC] p-8! flex justify-between">
           <h1 className="text-2xl font-bold text-white">CodeLeap Network</h1>
+          <button
+            className="cursor-pointer hover:opacity-80"
+            onClick={handleSignOut}
+          >
+            <SignOutIcon className="text-white text-2xl " />
+          </button>
         </header>
 
         <main className="p-5 flex-1 bg-white">
